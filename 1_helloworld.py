e2ed96baede0a37438d897aa5ee34d907846e0ac
Russel Aristo <russelaristo@gmail.com>
2023-03-13 10:06:12 +0800

    python

---
+++ 1_helloworld.py
@@ -0,0 +1,6 @@
+# 1. input
+
+# 2. process
+
+# 3. Output
+print("Hello World")
---
+++ 3_calculator_io.py
@@ -0,0 +1,13 @@
+#
+#calculator
+#
+
+#1. input
+x1 = input("Enter x1: ")
+x2 = input("Enter X2: ")
+
+#2. Process
+sum = int(x1) + int (x2)
+
+#3. Output
+print (f"sum: {sum}")
